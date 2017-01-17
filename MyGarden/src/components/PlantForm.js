1b3fcfad4e1cc9ea261ed1e40be660ed86bfe089
Panta <PantaD@haufe.com>
2017-01-17 12:49:10 +0200

    boc

--- MyGarden/src/components/PlantForm.js
+++ MyGarden/src/components/PlantForm.js
@@ -10,17 +10,57 @@ class EmployeeForm extends Component {
             <CardSection>
                 <TextBox
                     label="Name"
-                    placeholder="Ion Pop"
+                    placeholder="Rose"
                     onChangeText={value => this.props.plantUpdate({ prop: 'name', value })}
                     value={this.props.name}
                     />
             </CardSection>
             <CardSection>
                 <TextBox
-                    label="Phone"
-                    placeholder="0723456789"
-                    onChangeText={value => this.props.plantUpdate({ prop: 'phone', value })}
-                    value={this.props.phone}
+                    label="Plant description"
+                    placeholder="any of a genus of usually prickly shrubs with pinnate leaves and showy flowers"
+                    onChangeText={value => this.props.plantUpdate({ prop: 'description', value })}
+                    value={this.props.description}
+                    />
+            </CardSection>
+            <CardSection>
+                <TextBox
+                    label="Species"
+                    placeholder="Hulthemia"
+                    onChangeText={value => this.props.plantUpdate({ prop: 'species', value })}
+                    value={this.props.species}
+                    />
+            </CardSection>
+            <CardSection>
+                <TextBox
+                    label="Family"
+                    placeholder="Rosaceae"
+                    onChangeText={value => this.props.plantUpdate({ prop: 'family', value })}
+                    value={this.props.family}
+                    />
+            </CardSection>
+            <CardSection>
+                <TextBox
+                    label="Color"
+                    placeholder="pink"
+                    onChangeText={value => this.props.plantUpdate({ prop: 'color', value })}
+                    value={this.props.color}
+                    />
+            </CardSection>
+            <CardSection>
+                <TextBox
+                    label="Native range"
+                    placeholder="Asia"
+                    onChangeText={value => this.props.plantUpdate({ prop: 'native_range', value })}
+                    value={this.props.native_range}
+                    />
+            </CardSection>
+            <CardSection>
+                <TextBox
+                    label="Habitat"
+                    placeholder="Asia"
+                    onChangeText={value => this.props.plantUpdate({ prop: 'native_range', value })}
+                    value={this.props.native_range}
                     />
             </CardSection>
             <CardSection style={{ flexDirection: 'column' }}>
